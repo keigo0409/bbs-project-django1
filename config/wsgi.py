fed2fd967e32c3a76c7ecf46605d28715ea8eb9f
kamata keigo <k.kamata.sys23@morijyobi.ac.jp>
2025-06-06 14:51:02 +0900

    get_user_model()を　try内に移動

--- config/wsgi.py
+++ config/wsgi.py
@@ -12,16 +12,15 @@ from django.core.management import call_command
 from django.contrib.auth.models import get_user_model
 from django.db import OperationalError
 
-User = get_user_model()
 
 try:
     call_command('migrate',interactive=False)
     User = get_user_model()
     if not User.objects.filter(is_superuser=True).exists():
         import os
-        username=os.environ.get('DJANGO_SUPERUSER_NAME')
-        email=os.environ.get('DJANGO_SUPERUSER_EMAIL')
-        password=os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'morijyobi')
         User.objects.create_superuser(username=username,email=email,password=password)
 except OperationalError as e:
     print(f"Migrate error: {e}")
